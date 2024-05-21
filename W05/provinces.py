e26d53e774e4ecd1166d0607df235216cd3ae29a
randymz10 <randymz10@gmail.com>
2024-05-20 23:40:19 -0300

    Added new files

---
+++ W05/provinces.py
@@ -0,0 +1,18 @@
+def main():
+    province_list = read_list("provinces.txt")
+
+    print(province_list)
+
+
+def read_list(filename):
+    text_list = []
+
+    with open(filename, "rt") as text_file:
+        for line in text_file:
+            clean_line = line.strip()
+            text_list.append(clean_line)
+    return text_list;
+
+
+if __name__ == "__main__":
+    main()
